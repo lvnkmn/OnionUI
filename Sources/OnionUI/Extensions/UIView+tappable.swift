129d0d5ab14f53a673974451538c5bc457457c70
lvnkmn <2656075+lvnkmn@users.noreply.github.com>
2024-06-03 11:02:59 +0200

    Add UIView + tappable

---
+++ Sources/OnionUI/Extensions/UIView+tappable.swift
@@ -0,0 +1,27 @@
+//
+//  UIView+tappable.swift
+//  MidiJ
+//
+//  Created by me on 03/06/2024.
+//
+
+import UIKit
+import OnionUI
+
+#warning("Todo: Move to onionUI")
+public extension UIView {
+    func tappable(_ onTap: @escaping () -> ()) -> Control {
+        .init()
+        .addPinnedSubView{
+            self
+        }
+        .use { it in
+            it.addAction(
+                .init(handler: { _ in
+                    onTap()
+                }),
+                for: .touchUpInside
+            )
+        }
+    }
+}
