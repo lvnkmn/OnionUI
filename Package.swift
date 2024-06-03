984a0396acfa6b9378e3da5307f871c9d33bc831
lvnkmn <2656075+lvnkmn@users.noreply.github.com>
2024-06-03 15:19:14 +0200

    update lazy layout package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/lvnkmn/SourceSeeds", .upToNextMajor(from: Version(1, 0, 0))),
-        .package(url: "https://github.com/lvnkmn/LazyLayout", .upToNextMajor(from: Version(0, 0, 1)))
+        .package(url: "https://github.com/lvnkmn/LazyLayout", .upToNextMajor(from: Version(1, 0, 0)))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
