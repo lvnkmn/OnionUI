e58ed5a2522accb67138d367a3649023101d5ff9
lvnkmn <2656075+lvnkmn@users.noreply.github.com>
2024-06-03 12:27:46 +0200

    Update sourceseeds package

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/lvnkmn/SourceSeeds", .upToNextMajor(from: Version(0, 0, 1))),
+        .package(url: "https://github.com/lvnkmn/SourceSeeds", .upToNextMajor(from: Version(1, 0, 0))),
         .package(url: "https://github.com/lvnkmn/LazyLayout", .upToNextMajor(from: Version(0, 0, 1)))
     ],
     targets: [
